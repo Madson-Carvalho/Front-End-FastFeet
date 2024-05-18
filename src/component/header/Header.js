de4038e63e1e13e0d74cc8f03548833d7c641941
Madson-Carvalho <madsonraynnon@gmail.com>
2024-05-18 13:12:48 -0300

    Criando header

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -2,12 +2,14 @@ import "./Header.css";
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faHome} from "@fortawesome/free-solid-svg-icons";
+import logo from "../../assets/image/logo-removebg-preview.png";
 
 const Header = () => {
     return (
         <header>
+            <img src={logo} />
             <nav>
-                <a href={'#'}><FontAwesomeIcon icon={faHome}/></a>
+                <a href={'#'}><FontAwesomeIcon icon={faHome} inverse size="lg" /></a>
             </nav>
         </header>
     )
