d055427b92c3d6cef3aef97700d4c210d6cb5817
Madson-Carvalho <madsonraynnon@gmail.com>
2024-05-18 14:17:19 -0300

    Criados components para ajudar na construção da aplicação

---
+++ src/component/customInputSubmit/CustomInputSubmit.js
@@ -0,0 +1,9 @@
+import './CustomInputSubimit.css';
+
+const CustomInputSubmit = ({value}) => {
+    return (
+        <input className="defaultButton" type="submit" value={value}/>
+    )
+}
+
+export default CustomInputSubmit;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,15 @@
 import Header from "../../component/header/Header";
+import Footer from "../../component/footer/Footer";
+import Main from "../../component/main/Main";
 
 const Home = () => {
     return (
         <>
             <Header/>
+            <Main>
+
+            </Main>
+            <Footer/>
         </>
     )
 }
