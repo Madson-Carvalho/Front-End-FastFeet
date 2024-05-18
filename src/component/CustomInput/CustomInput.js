dae9a514f1c257354c3344f94e5b4a2794e7d85f
Madson-Carvalho <madsonraynnon@gmail.com>
2024-05-18 16:02:56 -0300

    Criado a tela de criação de usuário

--- src/component/CustomInput/CustomInput.js
+++ src/component/CustomInput/CustomInput.js
@@ -1,9 +1,22 @@
 import './CustomInput.css';
+import InputMask from "react-input-mask";
 
 const CustomInput = ({type, name, id, label}) => {
+    const isCPF = name === 'cpf';
+
     return (
         <div className="input-group defaultInputs">
-            <input type={type} name={name} id={id}/>
+            {isCPF ? (
+                <InputMask
+                    mask="999.999.999-99"
+                    id={id}
+                    name={name}
+                >
+                    {(inputProps) => <input {...inputProps} type={type} />}
+                </InputMask>
+            ) : (
+                <input type={type} name={name} id={id} />
+            )}
             <label className="label" htmlFor={id}>{label}</label>
         </div>
     )
