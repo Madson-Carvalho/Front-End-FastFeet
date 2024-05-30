9662fff0d0a53b886736348670bae037f0ad0f5a
madson_carvalho <madsonraynnon@gmail.com>
2024-05-29 21:35:32 -0300

    Criada a listagem de usuários com dados mocados

--- src/routes/PersonRoutes.js
+++ src/routes/PersonRoutes.js
@@ -3,12 +3,14 @@ import Home from "../pages/Home/Home";
 import NotFound from "../pages/notFound/NotFound";
 import RegisterUser from "../pages/registerUser/RegisterUser";
 import Login from "../pages/Login/Login";
+import ListUser from "../pages/listUser/ListUser";
 
 const PersonRoutes = () => {
     return (
         <Routes>
             <Route path={'/'} element={<Home/>}/>
             <Route path={'/create-user'} element={<RegisterUser/>}/>
+            <Route path='/users' element={<ListUser/>}/>
             <Route path='*' element={<NotFound/>}/>
             <Route path={'/login'} element={<Login/>}/>
         </Routes>
