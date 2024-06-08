4a9f8ff27a3564afbccd8ee35329bc5e8d5f8854
madson_carvalho <madsonraynnon@gmail.com>
2024-06-07 21:30:22 -0300

    Chamada a API externa do IBGE para busca de municípios por uf

--- src/component/customSelect/CustomSelect.js
+++ src/component/customSelect/CustomSelect.js
@@ -1,12 +1,12 @@
 import './CustomSelect.css';
 
-const CustomSelect = ({ name, id, options, label }) => {
+const CustomSelect = ({ name, id, options, label, onChange}) => {
     return (
         <div className="input-group defaultInputs">
-            <select name={name} id={id}>
+            <select name={name} id={id} onChange={onChange}>
                 <option value={''}>-- Selecione uma opção --</option>
                 {options.map(option => (
-                    <option value={option.value}>{option.name}</option>
+                    <option key={option.value} value={option.value}>{option.name}</option>
                 ))}
             </select>
             <label className="label" htmlFor={id}>{label}</label>
