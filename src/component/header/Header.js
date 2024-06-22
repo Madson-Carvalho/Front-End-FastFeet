f24c2c72647f1c30ee7cd888c492065d4f547a6a
Mayra-kauane <mayrakauane64@gmail.com>
2024-06-22 19:14:36 -0300

    fiz a listagem de destinatários e cadastro de destinatários integrado com back end

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,7 +1,7 @@
 import "./Header.css";
 import {Link} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHome, faUser, faUserPlus} from "@fortawesome/free-solid-svg-icons";
+import {faHome, faUser, faUserPlus, faLocationDot} from "@fortawesome/free-solid-svg-icons";
 import logo from "../../assets/image/logo-removebg-preview.png";
 
 const Header = () => {
@@ -11,6 +11,7 @@ const Header = () => {
             <nav>
                 <Link to={'/'} title="Home"><FontAwesomeIcon icon={faHome} inverse/> home</Link>
                 <Link to={'/users'}><FontAwesomeIcon icon={faUser} inverse/> usuário</Link>
+                <Link to={'/list-recipient'}><FontAwesomeIcon icon={faLocationDot} inverse/> destinatário</Link>
             </nav>
         </header>
     )
