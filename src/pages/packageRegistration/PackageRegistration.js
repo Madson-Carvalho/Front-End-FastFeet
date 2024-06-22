8a80aac617c4f1410ad858ebafe8cf7a3cf93ea1
RODRIGO TELES DONDÉ <rodrigo_donde@estudante.sesisenai.org.br>
2024-06-21 21:51:51 -0300

    Criados os inputs

--- src/pages/packageRegistration/PackageRegistration.js
+++ src/pages/packageRegistration/PackageRegistration.js
@@ -3,6 +3,7 @@ import Footer from "../../component/footer/Footer";
 import Main from "../../component/main/Main";
 import CustomInput from "../../component/CustomInput/CustomInput";
 import CustomInputSubmit from "../../component/customInputSubmit/CustomInputSubmit";
+import statusPackage from "../../utils/statusPackage";
 
 
 const PackageRegistration = () => {
@@ -11,10 +12,12 @@ const PackageRegistration = () => {
             <Header />
             <Main title="Cadastro de Encomenda">
                 <form className= 'base-form'>
-                    <CustomInput id='name' type='text' name='name' label='Nome' placeholder=" "/>
-                    <CustomInput id='cpf' type='text' name='cpf' label='CPF' placeholder=" "/>
-                    <CustomInput id='phone' type='phone' name='phone' label='Telefone' placeholder=" "/>
-                    <CustomInput id='email' type='email' name='email' label='E-mail' placeholder=" "/>
+                    <CustomInput id='recipient' type='text' name='recipient' label='Destinatário' placeholder=" "/>
+                    <CustomInput id='deliveryMan' type='text' name='deliveryMan' label='Entregador' placeholder=" "/>
+                    <CustomInput id='orderDate' type='date' name='orderDate' label='Data do Pedido' placeholder=" "/>
+                    <CustomInput id='expectedDate' type='date' name='expectedDate' label='Data prevista para entrega' placeholder=" "/>
+                    <CustomInput id='deliveryDate' type='date' name='deliveryDate' label='Data para realizar entrega' placeholder=" "/>
+                    <CustomSelect id='status' name='status' label='Status da encomenda' options={statusPackage} required={true}/>
                     <CustomInput id='address' type='text' name='address' label='Endereço' placeholder=" "/>
                     <CustomInputSubmit value='Salvar' />
                 </form>
